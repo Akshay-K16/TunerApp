64fef0b153dc277fd7175b1ad67119b235408ddd
Akshay Khemaney <khemaney.a@etoncollege.org.uk>
2021-09-20 13:10:38 +0100

    Initial Commit

---
+++ TunerApp/TunerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunerAppApp.swift
+//  TunerApp
+//
+//  Created by Khemaney, Akshay (SPH) on 20/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct TunerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
